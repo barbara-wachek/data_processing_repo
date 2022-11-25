fcbae728375da5cae70c8ab51cc22ee39e8ae1ef
Basia <barbara.wachek@gmail.com>
2022-11-25 14:13:02 +0100

    2022-11-25

--- zadanie_od_CR_API_BN_2022-10-28.py
+++ zadanie_od_CR_API_BN_2022-10-28.py
@@ -11,19 +11,20 @@
 #%% ROZWIĄZANIE
 import requests
 import pandas as pd
+from pprint import pprint
 
 
 author = 'Borges, Jorge Luis (1899-1986)'
 link = 'https://data.bn.org.pl/api/networks/bibs.json?'
 data_01 = requests.get(link, params = {'author': author, 'limit':100}).json()
-data_02 = requests.get(data_01['nextPage'], params = {'author': author, 'limit':100}).json()
-data_03 = requests.get(data_02['nextPage'], params = {'author': author, 'limit':100}).json()
-data_04 = requests.get(data_03['nextPage'], params = {'author': author, 'limit':100}).json()
+data_02 = requests.get(data_01['nextPage']).json()
+data_03 = requests.get(data_02['nextPage']).json()
+data_04 = requests.get(data_03['nextPage']).json()
 
 all_bibs = data_01['bibs'] + data_02['bibs'] + data_03['bibs']
 
 
-# Drugie rozwiązanie: 
+# Drugie rozwiązanie (krótsze)
 
 data = requests.get('https://data.bn.org.pl/api/networks/bibs.json?', params = {'author': 'Borges, Jorge Luis (1899-1986)', 'limit':100}).json()
 bibs = data['bibs']
@@ -33,9 +34,16 @@ while data['nextPage'] != '':
     bibs = bibs + data['bibs']
 
 
+#2022-11-25 ZADANIE OD CR KOLEJNE
+#Efekt w postaci DataFrame, który z tej listy 247 rekordó÷w robi tabele z 247 rekordami z MARC (tylko z pola MARC)
+#Mniej wiecej do takiej formy: https://docs.google.com/spreadsheets/d/1QMM1T0PFmcuQZLxhtM-qak6OXAsD39gYZdrgUNiqSUk/edit?pli=1#gid=0 #W pełni dynamiczna opcja
+
+
+
+#pprint(bibs)
 #pd_test = pd.DataFrame(bibs)
-pd_data = pd.json_normalize(bibs)
-pd_data.drop_duplicates
+#pd_data = pd.json_normalize(bibs)
+#pd_data.drop_duplicates
 
 
 
