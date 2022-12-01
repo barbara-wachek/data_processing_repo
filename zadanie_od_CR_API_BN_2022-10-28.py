614f403fd9e8adb45f8ceb5341b1501dd453107b
Basia <barbara.wachek@gmail.com>
2022-12-01 10:30:24 +0100

    2022-12-01

--- zadanie_od_CR_API_BN_2022-10-28.py
+++ zadanie_od_CR_API_BN_2022-10-28.py
@@ -14,6 +14,9 @@ import pandas as pd
 from pprint import pprint
 from pymarc import MARCReader
 import json
+import re
+
+
 
 
 author = 'Borges, Jorge Luis (1899-1986)'
@@ -39,19 +42,60 @@ while data['nextPage'] != '':
     bibs = bibs + data['bibs']
 
 
-marc_list_of_bibs = []
-marc_list_of_leader = []
+
+
+#2022-11-25 ZADANIE OD CR KOLEJNE
+#Efekt w postaci DataFrame, który z tej listy 247 rekordó÷w robi tabele z 247 rekordami z MARC (tylko z pola MARC)
+#Mniej wiecej do takiej formy: https://docs.google.com/spreadsheets/d/1QMM1T0PFmcuQZLxhtM-qak6OXAsD39gYZdrgUNiqSUk/edit?pli=1#gid=0 #W pełni dynamiczna opcja
+
+#Ciąg dalszy - tabela
+# marc_list_of_bibs = []
+# marc_list_of_leader = []
+
+# for x in bibs:
+#     marc_list_of_bibs.append(x['marc']['fields'])
+#     marc_list_of_leader.append(x['marc']['leader'])
+
+
+# marc_list_of_bibs #Może przekonwertować to na jsona? I potem użyć json_normalize? Na końcu dodać kolumnę leader
+
+
+
+marc_list = []
 
 for x in bibs:
-    marc_list_of_bibs.append(x['marc']['fields'])
-    marc_list_of_leader.append(x['marc']['leader'])
+    marc_list.append(x['marc'])
 
 
-marc_list_of_bibs #Może przekonwertować to na jsona? I potem użyć json_normalize? Na końcu dodać kolumnę leader
-records = [x for x in marc_list_of_bibs]   
+marc_normalize = pd.json_normalize(marc_list, sep=',') #Może później?
+
+
+columns_name = []
+
+
+
+# with open(f'marc_Borges.json', 'w', encoding='utf-8') as f:
+#     json.dump(marc_list, f)     
+                    
+# jsonStr = json.dumps(marc_list)
+# print(jsonStr)   
+
+# dir(data)
+# data = requests.get('https://data.bn.org.pl/api/networks/bibs.marc?', params = {'author': 'Borges, Jorge Luis (1899-1986)', 'limit':100}).text
+# bibs = data['bibs']
+
+# while data['nextPage'] != '':
+#     data = requests.get(data['nextPage']).json()
+#     bibs = bibs + data['bibs']
+
+
+# reader = MARCReader(data, to_unicode = True, force_utf8 = True)
+
+# dir(reader)
+
 
 names_of_columns = []
-for x in records:
+for x in marc_list_of_bibs:
     for y in x:
         for k,v in y.items():
             if k not in names_of_columns:
@@ -59,9 +103,32 @@ for x in records:
 
 #Jak wykorzystać w odpowiednim miejscu nazwy kolumny?       
         
-        
+  
+
 
 df_marc_list_of_bibs = pd.DataFrame(marc_list_of_bibs, columns=[])
+
+
+
+
+
+import collections
+
+def flatten(d, parent_key='', sep='_'):
+    items = []
+    for k, v in d.items():
+        new_key = parent_key + sep + k if parent_key else k
+        if isinstance(v, collections.MutableMapping):
+            items.extend(flatten(v, new_key, sep=sep).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
+
+>>> flatten({'a': 1, 'c': {'a': 2, 'b': {'x': 5, 'y' : 10}}, 'd': [1, 2, 3]})
+{'a': 1, 'c_a': 2, 'c_b_x': 5, 'd': [1, 2, 3], 'c_b_y': 10}
+
+
+
 
 # jsonString = json.dumps(marc_list_of_bibs) 
     
@@ -149,9 +216,7 @@ for element in marc_list_of_bibs:
 Series.str.split().
 
 
-#2022-11-25 ZADANIE OD CR KOLEJNE
-#Efekt w postaci DataFrame, który z tej listy 247 rekordó÷w robi tabele z 247 rekordami z MARC (tylko z pola MARC)
-#Mniej wiecej do takiej formy: https://docs.google.com/spreadsheets/d/1QMM1T0PFmcuQZLxhtM-qak6OXAsD39gYZdrgUNiqSUk/edit?pli=1#gid=0 #W pełni dynamiczna opcja
+
 
 
 
