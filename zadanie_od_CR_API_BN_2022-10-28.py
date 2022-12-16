dca72537eda1f6c8338bd538455ec53eddf529c2
Basia <barbara.wachek@gmail.com>
2022-12-16 13:53:32 +0100

    laptop

--- zadanie_od_CR_API_BN_2022-10-28.py
+++ zadanie_od_CR_API_BN_2022-10-28.py
@@ -11,8 +11,6 @@
 #%% ROZWIĄZANIE
 import requests
 import pandas as pd
-from pprint import pprint
-from pymarc import MARCReader
 import json
 import re
 from flatten_json import flatten
@@ -32,8 +30,6 @@ all_bibs = data_01['bibs'] + data_02['bibs'] + data_03['bibs']
 
 
 
-
-
 # Drugie rozwiązanie (krótsze)
 
 data = requests.get('https://data.bn.org.pl/api/networks/bibs.json?', params = {'author': 'Borges, Jorge Luis (1899-1986)', 'limit':100}).json()
@@ -44,12 +40,9 @@ while data['nextPage'] != '':
     bibs = bibs + data['bibs']
 
 
-
-
 #2022-11-25 ZADANIE OD CR KOLEJNE
 #Efekt w postaci DataFrame, który z tej listy 247 rekordó÷w robi tabele z 247 rekordami z MARC (tylko z pola MARC)
 #Mniej wiecej do takiej formy: https://docs.google.com/spreadsheets/d/1QMM1T0PFmcuQZLxhtM-qak6OXAsD39gYZdrgUNiqSUk/edit?pli=1#gid=0 #W pełni dynamiczna opcja
-
 
 
 all_marc_series = []
@@ -64,9 +57,9 @@ data_frame_marc.rename(columns={'leader': 'LDR'}, inplace=True)
 #Ustawienie bardziej zrozumiałych nazw kolumn:
 format_fields = r'(fields\_\d{1,2}\_)(\d{3})$'
 format_ind = r'(fields\_\d{1,2}\_)(\d{3}\_)(ind\d)'
-format_subfields = r'(fields\_\d{1,2}\_)(\d{3}\_)(subfields\_\d\_\w)'
+format_subfields = r'(fields\_\d{1,2}\_)(\d{3}\_)(subfields\_\d{1,2}\_\w)'
 
-for x in data_frame_marc.columns:
+for x in data_frame_marc.columns:    
     if re.match(format_fields, x):
         new_name = re.sub(r'(fields\_\d{1,2}\_)(\d{3})$', r'\2', x)
         data_frame_marc.rename(columns={x: new_name}, inplace=True)
@@ -74,60 +67,73 @@ for x in data_frame_marc.columns:
         new_name = re.sub(r'(fields\_\d{1,2}\_)(\d{3}\_)(ind\d)', r'\2\3', x)
         data_frame_marc.rename(columns={x: new_name}, inplace=True)
     elif re.match(format_subfields, x):
-        new_name = re.sub(r'(fields\_\d{1,2}\_)(\d{3}\_)(subfields\_\d\_\w)', r'\2\3', x)
+        new_name = re.sub(r'(fields\_\d{1,2}\_)(\d{3}\_)(subfields\_\d{1,2}\_\w)', r'\2\3', x)
         data_frame_marc.rename(columns={x: new_name}, inplace=True)
 
 
+#column_list = data_frame_marc.columns %Do sprawdzenia nazw kolumn
 
-format_column_name = r'(\d{3})(\_)?(ind|subfields)?(\_)?(\d)(\_)?(\w)?'
-format_marc_field = r'(^\d{3})(?=.*)'
+# test_list = []
 
+# for column_name in data_frame_marc.columns:
+#     number_of_column = re.sub(r'(\d{3})(\_)?(ind|subfields)?(\_)?(\d)(\_)?(\w)?', r'\1', column_name)
+#     if number_of_column not in test_list:
+#         test_list.append(number_of_column)
+#         data_frame_marc.rename(columns={column_name:number_of_column}, inplace=True)
+#         if 'ind' in column_name:
+#             data_frame_marc[number_of_column] = data_frame_marc[number_of_column].replace(' ', '\\')
+#     elif number_of_column in test_list: 
+#         data_frame_marc.rename(columns={column_name:number_of_column}, inplace=True)
+#         if 'ind' in column_name: 
+#             data_frame_marc[number_of_column] = data_frame_marc[number_of_column].replace(' ', '\\')
+            
+# test_list = []
 
-
-test_list = []
-
-for column_name in data_frame_marc.columns:
-    number_of_column = re.sub(r'(\d{3})(\_)?(ind|subfields)?(\_)?(\d)(\_)?(\w)?', r'\1', column_name)
-    if number_of_column not in test_list:
-        test_list.append(number_of_column)
-        data_frame_marc.rename(columns={column_name:number_of_column}, inplace=True)
-        if 'ind' in column_name:
-            data_frame_marc[number_of_column].replace('', "\\")
-            
-            
-            
-            
-            for data in data_frame_marc[number_of_column]:  #musi iterować po komórkach, zeby zmianić ich wartoć - nie po całej kolumnie
-                if not re.search(r'\d', data):
-                    data_frame_marc[number_of_column] = '\\'        
-    elif number_of_column in test_list: 
-        if 'ind' in column_name:
-            if not data_frame_marc[column_name].str.contains('\d', flags=re.I, regex=True):
-                data_frame_marc[column_name] = '\\'      
-            data_frame_marc.rename(columns={column_name:number_of_column}, inplace=True)
-        # elif 'subfields' in column_name:
-        #     data_frame_marc[column_name] = f'${data_frame_marc[column_name]}'
-        #     data_frame_marc.rename(columns={column_name:number_of_column}, inplace=True)
-            
-            
-            
-            
-            
-    #     data_frame_marc[number_of_column] = data_frame_marc[number_of_column] + data_frame_marc[column_name].convert_dtypes(convert_string=True)
-
-
-# Define a function
-# def add_dolar_to_subfield():
-#     for row in data_frame_marc: 
-#         for column in data_frame_marc.columns:
-#             if 'subfield' in column and data_frame_marc[column].notna(): 
-#                 data_frame_marc[column] = f'${data_frame_marc[column]}'
+# for column_name in data_frame_marc.columns:
+#     number_of_column = re.sub(r'(\d{3})(\_)?(ind|subfields)?(\_)?(\d)(\_)?(\w)?', r'\1', column_name)
+#     data_frame_marc.rename(columns={column_name:number_of_column}, inplace=True)
+#     if number_of_column not in test_list:
+#         test_list.append(number_of_column)
         
+#     if 'ind' in column_name:
+#         data_frame_marc[column_name] = data_frame_marc[column_name].replace(' ', '\\')
+#     if 'subfields' in column_name: 
+#         number_and_letter_of_subfield = re.sub(r'(\d{3})(\_)(subfields)(\_)(\d{1,2})(\_)(\w)', r'\5\7', column_name)
+        
+#         try:
+#             data_frame_marc[column_name] = data_frame_marc[column_name].str.replace(r'(^.*$)', r'$\1').str.replace(r'(\$)(.*)', r'\g<1>'+number_and_letter_of_subfield+r'\g<2>')
+#         except AttributeError:
+#             data_frame_marc[column_name] = data_frame_marc[column_name].apply(lambda x: x.str.replace(r'(^.*$)', r'$'+number_and_letter_of_subfield+ r'\g<1>❦')) 
+           
+   
 
-
-# data_frame_marc.apply(add_dolar_to_subfield)
-
-
+    
+#Wersja ostateczna:             
+     
+test_list = []
+for column_name in data_frame_marc.columns:
+    if 'ind' in column_name:
+        data_frame_marc[column_name] = data_frame_marc[column_name].replace(' ', '\\')
+        if column_name not in test_list: 
+            test_list.append(column_name)
+        
+    if 'subfields' in column_name: 
+        number_and_letter_of_subfield = re.sub(r'(\d{3})(\_)(subfields)(\_)(\d{1,2})(\_)(\w)', r'\5\7', column_name)
+        if column_name not in test_list: #Jesli nazwy kolumny nie ma na liscie testowej tzn., ze pierwszy raz analizujemy kolumnę o takiej nazwie. Musimy dodać ją do listy testowej, a nastepnie mozemy podejsc do analizy: sprobowac dodac odpowiednie znaki na poczatku wartosci z poszczegolnych komorek tej kolumny. Jesli wyskoczy blad (AttributeError) oznacza to, ze mamy wiecej kolumn o tej samej nazwie, bo kod interpretuje to jako DataFrame. Wtedy dodajemy znaczek ❦ na koncu kazdej komorki z takich kolumn. Wazne tez, zeby po w kolejnej iteracji gdy probujemy analizowac kolejna kolumne o tej samej nazwie, zeby po prostu zostawiło ją - bo juz są na niej naniesione zmiany. 
+            test_list.append(column_name)
+            try:
+                data_frame_marc[column_name] = data_frame_marc[column_name].str.replace(r'(^.*$)', r'$\1').str.replace(r'(\$)(.*)', r'\g<1>'+number_and_letter_of_subfield+r'\g<2>')
+            except AttributeError:
+                data_frame_marc[column_name] = data_frame_marc[column_name].apply(lambda x: x.str.replace(r'(^.*$)', r'$'+number_and_letter_of_subfield+ r'\g<1>❦')) 
+                
+        if column_name in test_list: 
+            pass
+            
+ 
+            
+#Dlaczego niektóre powtarzające się kolumny nie są obok siebie? 
+#Kolejny krok: połączyć kolumny o tych samych poczatkach nazw (trzy pierwsze cyfry)
+#Poprawić uzyćie znaczka ❦. Cos sie nie zgadza - moze to nie powinno byc dodane w tej funkcji, ale w kolejnej. BO teraz dodaje po porust w obrębie kolumny nie zwazajac na kontekt calego rekordu? (Np. 7 rekord pole subfields_35 w tabeli ) PRawdopodobnie trzeba bedzie wywalic ten znaczek z powyzszego kodu i dodac go dopiero po scaleniu kolumn (wtedy bedzie sprawdzenie czy dana komorka zawiera kilka $+liczba+litera - to znaczy, ze ma kilka tych samych podpól?)
 
 
     
