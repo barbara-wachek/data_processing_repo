5b92fddd5ef85b64a7a7f0d1bed758b5b758047e
Basia <barbara.wachek@gmail.com>
2022-12-19 13:14:52 +0100

    2022-12-19

--- zadanie_od_CR_API_BN_2022-10-28.py
+++ zadanie_od_CR_API_BN_2022-10-28.py
@@ -17,21 +17,6 @@ from flatten_json import flatten
 import tqdm
 
 
-
-
-author = 'Borges, Jorge Luis (1899-1986)'
-link = 'https://data.bn.org.pl/api/networks/bibs.json?'
-data_01 = requests.get(link, params = {'author': author, 'limit':100}).json()
-data_02 = requests.get(data_01['nextPage']).json()
-data_03 = requests.get(data_02['nextPage']).json()
-data_04 = requests.get(data_03['nextPage']).json()
-
-all_bibs = data_01['bibs'] + data_02['bibs'] + data_03['bibs']
-
-
-
-# Drugie rozwiązanie (krótsze)
-
 data = requests.get('https://data.bn.org.pl/api/networks/bibs.json?', params = {'author': 'Borges, Jorge Luis (1899-1986)', 'limit':100}).json()
 bibs = data['bibs']
 
@@ -56,59 +41,23 @@ data_frame_marc.rename(columns={'leader': 'LDR'}, inplace=True)
 
 #Ustawienie bardziej zrozumiałych nazw kolumn:
 format_fields = r'(fields\_\d{1,2}\_)(\d{3})$'
-format_ind = r'(fields\_\d{1,2}\_)(\d{3}\_)(ind\d)'
-format_subfields = r'(fields\_\d{1,2}\_)(\d{3}\_)(subfields\_\d{1,2}\_\w)'
+format_ind = r'(fields\_\d{1,3}\_)(\d{3}\_)(ind\d)'
+format_subfields = r'(fields\_\d{1,3}\_)(\d{3}\_)(subfields\_\d{1,2}\_\w)'
 
 for x in data_frame_marc.columns:    
     if re.match(format_fields, x):
         new_name = re.sub(r'(fields\_\d{1,2}\_)(\d{3})$', r'\2', x)
         data_frame_marc.rename(columns={x: new_name}, inplace=True)
     elif re.match(format_ind, x):
-        new_name = re.sub(r'(fields\_\d{1,2}\_)(\d{3}\_)(ind\d)', r'\2\3', x)
+        new_name = re.sub(r'(fields\_\d{1,3}\_)(\d{3}\_)(ind\d)', r'\2\3', x)
         data_frame_marc.rename(columns={x: new_name}, inplace=True)
     elif re.match(format_subfields, x):
-        new_name = re.sub(r'(fields\_\d{1,2}\_)(\d{3}\_)(subfields\_\d{1,2}\_\w)', r'\2\3', x)
+        new_name = re.sub(r'(fields\_\d{1,3}\_)(\d{3}\_)(subfields\_\d{1,2}\_\w)', r'\2\3', x)
         data_frame_marc.rename(columns={x: new_name}, inplace=True)
 
 
-#column_list = data_frame_marc.columns %Do sprawdzenia nazw kolumn
+column_list = data_frame_marc.columns #Do sprawdzenia nazw kolumn
 
-# test_list = []
-
-# for column_name in data_frame_marc.columns:
-#     number_of_column = re.sub(r'(\d{3})(\_)?(ind|subfields)?(\_)?(\d)(\_)?(\w)?', r'\1', column_name)
-#     if number_of_column not in test_list:
-#         test_list.append(number_of_column)
-#         data_frame_marc.rename(columns={column_name:number_of_column}, inplace=True)
-#         if 'ind' in column_name:
-#             data_frame_marc[number_of_column] = data_frame_marc[number_of_column].replace(' ', '\\')
-#     elif number_of_column in test_list: 
-#         data_frame_marc.rename(columns={column_name:number_of_column}, inplace=True)
-#         if 'ind' in column_name: 
-#             data_frame_marc[number_of_column] = data_frame_marc[number_of_column].replace(' ', '\\')
-            
-# test_list = []
-
-# for column_name in data_frame_marc.columns:
-#     number_of_column = re.sub(r'(\d{3})(\_)?(ind|subfields)?(\_)?(\d)(\_)?(\w)?', r'\1', column_name)
-#     data_frame_marc.rename(columns={column_name:number_of_column}, inplace=True)
-#     if number_of_column not in test_list:
-#         test_list.append(number_of_column)
-        
-#     if 'ind' in column_name:
-#         data_frame_marc[column_name] = data_frame_marc[column_name].replace(' ', '\\')
-#     if 'subfields' in column_name: 
-#         number_and_letter_of_subfield = re.sub(r'(\d{3})(\_)(subfields)(\_)(\d{1,2})(\_)(\w)', r'\5\7', column_name)
-        
-#         try:
-#             data_frame_marc[column_name] = data_frame_marc[column_name].str.replace(r'(^.*$)', r'$\1').str.replace(r'(\$)(.*)', r'\g<1>'+number_and_letter_of_subfield+r'\g<2>')
-#         except AttributeError:
-#             data_frame_marc[column_name] = data_frame_marc[column_name].apply(lambda x: x.str.replace(r'(^.*$)', r'$'+number_and_letter_of_subfield+ r'\g<1>❦')) 
-           
-   
-
-    
-#Wersja ostateczna:             
      
 test_list = []
 for column_name in data_frame_marc.columns:
@@ -124,27 +73,47 @@ for column_name in data_frame_marc.columns:
             try:
                 data_frame_marc[column_name] = data_frame_marc[column_name].str.replace(r'(^.*$)', r'$\1').str.replace(r'(\$)(.*)', r'\g<1>'+number_and_letter_of_subfield+r'\g<2>')
             except AttributeError:
-                data_frame_marc[column_name] = data_frame_marc[column_name].apply(lambda x: x.str.replace(r'(^.*$)', r'$'+number_and_letter_of_subfield+ r'\g<1>❦')) 
+                data_frame_marc[column_name] = data_frame_marc[column_name].apply(lambda x: x.str.replace(r'(^.*$)', r'$'+number_and_letter_of_subfield+ r'\g<1>')) 
                 
         if column_name in test_list: 
             pass
             
  
-            
+ #  ❦        
+   
+   
+   
 #Dlaczego niektóre powtarzające się kolumny nie są obok siebie? 
 #Kolejny krok: połączyć kolumny o tych samych poczatkach nazw (trzy pierwsze cyfry)
-#Poprawić uzyćie znaczka ❦. Cos sie nie zgadza - moze to nie powinno byc dodane w tej funkcji, ale w kolejnej. BO teraz dodaje po porust w obrębie kolumny nie zwazajac na kontekt calego rekordu? (Np. 7 rekord pole subfields_35 w tabeli ) PRawdopodobnie trzeba bedzie wywalic ten znaczek z powyzszego kodu i dodac go dopiero po scaleniu kolumn (wtedy bedzie sprawdzenie czy dana komorka zawiera kilka $+liczba+litera - to znaczy, ze ma kilka tych samych podpól?)
-
+#Poprawić uzyćie znaczka ❦. Cos sie nie zgadza - moze to nie powinno byc dodane w tej funkcji, ale w kolejnej. BO teraz dodaje po porust w obrębie kolumny nie zwazajac na kontekst calego rekordu? (Np. 7 rekord pole subfields_35 w tabeli ) Prawdopodobnie trzeba bedzie wywalic ten znaczek z powyzszego kodu i dodac go dopiero po scaleniu kolumn (wtedy bedzie sprawdzenie czy dana komorka zawiera kilka $+liczba+litera - to znaczy, ze ma kilka tych samych podpól?)
 
     
-    
+#Dodanie znaczka ❦ i połączenie kolumn o tej samej liczbie początkowej w nazwie   
+
+# format_column_name = f'(\d{3})(.*)?'
+# format_column_subfields = '(\d{3})(\_subfields\_\d{1,2}\_\w)'
+
+
+# for column_name in data_frame_marc.columns:
+#     if data_frame_marc.columns.duplicated().any(): 
+#         data_frame_marc[column_name] = data_frame_marc[column_name].str.replace(r'(^.*$)', r'\g<1>'+'❦ ')
 
 
 
 
+#DO przemyslenia
 
-
-
+for column_name in data_frame_marc.columns:
+    column_name = '020_ind1'
+    if re.match(r'(\d{3})(\_.*)', column_name):
+        new_column = re.sub(r'(\d{3})(\_.*)?', r'\1', column_name)
+        if new_column in data_frame_marc.columns:
+            data_frame_marc[new_column] = data_frame_marc[new_column] + data_frame_marc[column_name]
+            pass
+            #data_frame_marc.drop(columns=[column_name], inplace=True)
+        else:
+            data_frame_marc[new_column] = data_frame_marc[column_name]
+            #data_frame_marc.drop(columns=[column_name], inplace=True)
 
 
 
