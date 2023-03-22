696a18d84a61c010693ba305e27a1115b06166ec
Basia <barbara.wachek@gmail.com>
2023-03-22 14:09:08 +0100

    2023-03-22

--- BazHum.py
+++ BazHum.py
@@ -95,55 +95,48 @@ with pd.ExcelWriter(f"BazHum.xlsx", engine='xlsxwriter', options={'strings_to_ur
 #%% Pobranie pdfów z linków, gdzie decyzja jest TAK (puste pole)
    
 #Link = 'https://docs.google.com/spreadsheets/d/1ZN5UdQwla4lLvyXZk_2Ug-PAWUrcarEmT0nqSTwdy5Y/edit#gid=66593903'
-path_laptop = "C:\\Users\\Barbara Wachek\\Desktop\\BazHum – czasopisma do pozyskania - web scraping.csv"
 path_desktop = "C:\\Users\\PBL_Basia\\Desktop\\BazHum – czasopisma do pozyskania - web scraping.csv"
 
 
-with open("C:\\Users\\Barbara Wachek\\Desktop\\BazHum – czasopisma do pozyskania - web scraping.csv", 'r', encoding='utf-8') as file:
+with open("C:\\Users\\PBL_Basia\\Desktop\\BazHum – czasopisma do pozyskania - web scraping.csv", 'r', encoding='utf-8') as file:
     df = pd.read_csv(file) 
-    
-#df.shape
-#df_negative_decisions = df[df['Decyzja'].notna()]
-df_positive_decisions = df[df['Decyzja'].isna()]
-df_positive_decisions['Plik PDF'] = np.nan
 
-test_df = df_positive_decisions[500:600]
+df['Plik PDF'] = np.nan   
+df = df.drop(columns=['Unnamed: 6'])
+df_positive_decisions = df[df['Decyzja'].isna()]
 
 
 list_of_files_paths = []
+list_of_bugs = []
 
-def create_list_of_files_paths(link):
+#Po pętli na linkach z df_positive_decisions['PDF'] ten sam kod puscic tez dla linkow zebranych ewentualnie w liscie list_of_bugs
+for link in tqdm(df_positive_decisions['PDF']):
+
+    try:
+        format_name = re.sub(r'(https?\:\/\/bazhum\.muzhp\.pl\/media\/\/files\/)(.*\/)(.*)(\.pdf$)', r'\3', link)
+        file_path = f'C:\\Users\\PBL_Basia\\Documents\\My scripts\\BazHum_PDF-y\\{format_name}.pdf'
+        filename = Path(file_path)
     
-#for link in tqdm(test_df['PDF']):
-    link = 'http://bazhum.muzhp.pl/media//files/Teksty_teoria_literatury_krytyka_interpretacja/Teksty_teoria_literatury_krytyka_interpretacja-r1981-t-n6_(60)/Teksty_teoria_literatury_krytyka_interpretacja-r1981-t-n6_(60)-s105-121/Teksty_teoria_literatury_krytyka_interpretacja-r1981-t-n6_(60)-s105-121.pdf'
-    #link = 'http://bazhum.muzhp.pl/media//files/Acta_Universitatis_Lodziensis_Folia_Litteraria_Polonica/Acta_Universitatis_Lodziensis_Folia_Litteraria_Polonica-r2005-t7-n1/Acta_Universitatis_Lodziensis_Folia_Litteraria_Polonica-r2005-t7-n1-s303-315/Acta_Universitatis_Lodziensis_Folia_Litteraria_Polonica-r2005-t7-n1-s303-315.pdf'
-    #link = 'http://bazhum.muzhp.pl/media//files/Collectanea_Philologica/Collectanea_Philologica-r1999-t3/Collectanea_Philologica-r1999-t3-s155-159/Collectanea_Philologica-r1999-t3-s155-159.pdf'
-    
-    format_name = re.sub(r'(http\:\/\/bazhum\.muzhp\.pl\/media\/\/files\/)(.*\/)(.*)(\.pdf$)', r'\3', link)
-    #file_path = f'C:\\Users\\PBL_Basia\\Documents\\My scripts\\BazHum_PDF-y\\{format_name}.pdf' #desktop
-    file_path = f"C:\\Users\\Barbara Wachek\\Documents\\Python Scripts\\BazHum\\{format_name}.pdf" #laptop
-    
-    filename = Path(file_path)
-    response = requests.get(link)
-    filename.write_bytes(response.content)
-    
-    list_of_files_paths.append(file_path)
+        response = requests.get(link)
+        filename.write_bytes(response.content)
+        list_of_files_paths.append(file_path)
+        
+    except ConnectionError:
+        list_of_bugs.append(link)
+        
+    except TypeError:
+        pass
  
+#Usuniecie z listy sciezek ewentualnych duplikatów:
+list_of_files_paths = list(set(list_of_files_paths))       
     
- 
-#MAIN
-# list_of_files_paths = []
-# with ThreadPoolExecutor() as excecutor:
-#     list(tqdm(excecutor.map(create_list_of_files_paths, test_df['PDF']), total=len(test_df)))
-  
 
-
+#Zapisanie wszystkich plików na Dysku Google z przechowaniem linkow do tych plikow w zmiennej dict_of_drive_links, aby pozniej uzupelnic df
 dict_of_drive_links = {}   
 gauth = GoogleAuth()           
 drive = GoogleDrive(gauth)   
   
-upload_file_list = list_of_files_paths
-for upload_file in upload_file_list:
+for upload_file in tqdm(list_of_files_paths):
     format_name = re.sub('(C\:\\\\Users\\\\PBL_Basia\\\\Documents\\\\My scripts\\\\BazHum_PDF-y\\\\)(.*)', r'\2', upload_file)
     gfile = drive.CreateFile({'parents': [{'id': '1NgarB8afK-v7J9LjJORjegmd8-7Dw99w'}], 'title': format_name})  
     gfile.SetContentFile(upload_file)
@@ -151,35 +144,30 @@ for upload_file in upload_file_list:
     drive_link = gfile.metadata['alternateLink']
     dict_of_drive_links[format_name] = drive_link
 
+
+#Uzupełnienie danych w df_positive_decisions, aby dodac do df z wybranymi artykulami linki do plikow na Dysku Google: 
+for k,v in tqdm(dict_of_drive_links.items()):
+    df_positive_decisions.loc[df_positive_decisions['PDF'].str.endswith(k, na=False), 'Plik PDF'] = v
+     
+
+#Usun z df_positive_decisions kolumny Decyzja:
+df_positive_decisions = df_positive_decisions.drop(columns=['Decyzja'])
+
+#Zapisanie df do tabeli: 
+with pd.ExcelWriter(f"C:\\Users\\PBL_Basia\\Desktop\\BazHum_tabela_z_linkami_do_plikow_PDF.xlsx", engine='xlsxwriter', options={'strings_to_urls': False}) as writer:    
+    df_positive_decisions.to_excel(writer, index=False, encoding='utf-8')   
+    writer.save()
     
 
-#Uzupełnienie danych w df, aby dodac do poczatkowego dataframe'u linki do pdfów na dysku
+#Uzupełnienie pierwotnego df o brakujace informacje, aby otrzymać tę samą tabelę co na początku, ale uzupełnioną o tabelę z linkami do plików na dysku:
 
-for k,v in dict_of_drive_links.items():
-    # k = 'Collectanea_Philologica-r1995-t1-s119-130.pdf'
-    # v = 'https://drive.google.com/file/d/1ciFJ0ljohRL-GSdr0y5asxud-xEECIXA/view?usp=drivesdk'
-    #df_positive_decisions.loc[df_positive_decisions['PDF'].str.contains(k, na=False), 'Plik PDF'] = v
-    test_df.loc[test_df['PDF'].str.contains(k, na=False), 'Plik PDF'] = v
+for k,v in tqdm(dict_of_drive_links.items()):
+    df.loc[df['PDF'].str.endswith(k, na=False), 'Plik PDF'] = v
+     
+with pd.ExcelWriter(f"C:\\Users\\PBL_Basia\\Desktop\\BazHum_tabela_z_linkami_do_plikow_PDF(wszystkie_wiersze).xlsx", engine='xlsxwriter', options={'strings_to_urls': False}) as writer:    
+    df.to_excel(writer, index=False, encoding='utf-8')   
+    writer.save()
     
-      
-
-
-
-#Usun z finalnego DF kolumnę Unnamed: 6
-   
-
-
-
-
-
-
-
-    
-
-
-
-
-
 
 
 
