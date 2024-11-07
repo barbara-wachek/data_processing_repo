eaea109911fad50d31715a9417b3a6c4c0c1cd36
Barbara Wachek <barbara.wachek@gmail.com>
2024-11-07 13:18:33 +0100

    2024-11-07

--- KUP.py
+++ KUP.py
@@ -1,3 +1,8 @@
+#%%% Description
+
+
+
+#%% Import
 import cx_Oracle
 import pandas as pd
 import os
@@ -5,24 +10,17 @@ import datetime
 import random
 
 #%% System variables
-db_password = os.environ.get('DB_PASSWORD')
-db_user = os.environ.get('DB_USER')
-db_host = os.environ.get('DB_HOST')
-db_port = os.environ.get('DB_PORT')
-db_sid = os.environ.get('DB_SID')
+# db_password = os.environ.get('DB_PASSWORD')
+# db_user = os.environ.get('DB_USER')
+# db_host = os.environ.get('DB_HOST')
+# db_port = os.environ.get('DB_PORT')
+# db_sid = os.environ.get('DB_SID')
 
-#%% Connect with DB
+#%% VARIABLES
 
-cx_Oracle.init_oracle_client(lib_dir=r"C:\Users\PBL_Basia\Desktop\SQL\sqldeveloper\instantclient_19_6")
-dsn_tns = cx_Oracle.makedsn(db_host, db_port, service_name=db_sid)
-connection = cx_Oracle.connect(user=db_user, password=db_password, dsn=dsn_tns, encoding='windows-1250')
-
-
-#%% Query
-
-date_start = '2020/01/01'
+DATE_START = '2020/01/01'
 date_end = datetime.date.today().strftime('%Y/%m/%d')
-user_pbl = 'KAROLINA'
+user_pbl = input('Wybierz nazwę użytkownika: ')
 
 query = f'''
 select z.za_zapis_id id, z.za_type typ, rz.rz_nazwa rodzaj, dz.dz_nazwa dzial, a.am_nazwisko autor_nazwisko, a.am_imie autor_imie, z.za_tytul tytul, z.za_opis_wspoltworcow wspoltworcy, z.za_adnotacje adnotacja, zr.zr_tytul czasopismo, z.za_zrodlo_rok rok, z.za_zrodlo_nr numer, z.za_zrodlo_str strony, z.za_uzytk_wpisal, zr.zr_zrodlo_id, z.za_uzytk_wpis_data
@@ -32,7 +30,92 @@ join IBL_OWNER.pbl_autorzy a on za.zaam_am_autor_id=a.am_autor_id
 join IBL_OWNER.pbl_zrodla zr on zr.zr_zrodlo_id=z.za_zr_zrodlo_id
 join IBL_OWNER.pbl_dzialy dz on dz.dz_dzial_id=z.za_dz_dzial1_id
 join IBL_OWNER.pbl_rodzaje_zapisow rz on rz.rz_rodzaj_id=z.za_rz_rodzaj1_id
-where z.za_uzytk_wpis_data between TO_DATE('{date_start}','YYYY-MM-DD') and TO_DATE('{date_end}','YYYY-MM-DD')
+where z.za_uzytk_wpis_data between TO_DATE('{DATE_START}','YYYY-MM-DD') and TO_DATE('{date_end}','YYYY-MM-DD')
+and z.za_ro_rok >= 2004
+and z.za_uzytk_wpisal like '{user_pbl}'
+and (z.za_zr_zrodlo_id is not null or z.za_zr_zrodlo_id not like 'x')
+order by z.za_zapis_id
+'''
+
+
+
+#%% Functions
+
+def connect_to_database():
+    """Connects to the Oracle database and returns a connection object."""
+    cx_Oracle.init_oracle_client(lib_dir=r"C:\Users\PBL_Basia\Desktop\SQL\sqldeveloper\instantclient_19_6")
+    dsn_tns = cx_Oracle.makedsn(os.environ.get('DB_HOST'), os.environ.get('DB_PORT'), service_name=os.environ.get('DB_SID'))
+
+    try:
+        connection = cx_Oracle.connect(user=os.environ.get('DB_USER'), password=os.environ.get('DB_PASSWORD'), dsn=dsn_tns, encoding='windows-1250')
+        return connection
+    except cx_Oracle.Error as error:
+        print(f"Error connecting to database: {error}")
+        return None
+
+
+# cx_Oracle.init_oracle_client(lib_dir=r"C:\Users\PBL_Basia\Desktop\SQL\sqldeveloper\instantclient_19_6")
+# dsn_tns = cx_Oracle.makedsn(db_host, db_port, service_name=db_sid)
+
+# connection = cx_Oracle.connect(user=db_user, password=db_password, dsn=dsn_tns, encoding='windows-1250')
+
+def execute_query(connection, query, user_pbl):
+    """Executes a given SQL query and returns the result as a pandas DataFrame."""
+    cursor = connection.cursor()
+    cursor.execute(query)
+    result = cursor.fetchall()
+    column_names = [desc[0] for desc in cursor.description]
+    df = pd.DataFrame(result, columns=column_names)   
+
+    return df
+
+
+
+
+#%% Main
+
+if __name__ == "__main__":
+    with connect_to_database() as conn:
+        if conn:
+            df = execute_query(conn, query, user_pbl)
+            print(df)
+        else:
+            print("Failed to connect to database.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#%% Query - moj kod
+
+
+
+
+
+
+#Ponizsze zapytanie generuje wszystkie zapisy danej osoby w okreslonym czasie.
+
+query = f'''
+select z.za_zapis_id id, z.za_type typ, rz.rz_nazwa rodzaj, dz.dz_nazwa dzial, a.am_nazwisko autor_nazwisko, a.am_imie autor_imie, z.za_tytul tytul, z.za_opis_wspoltworcow wspoltworcy, z.za_adnotacje adnotacja, zr.zr_tytul czasopismo, z.za_zrodlo_rok rok, z.za_zrodlo_nr numer, z.za_zrodlo_str strony, z.za_uzytk_wpisal, zr.zr_zrodlo_id, z.za_uzytk_wpis_data
+from pbl_zapisy z
+join IBL_OWNER.pbl_zapisy_autorzy za on za.zaam_za_zapis_id=z.za_zapis_id
+join IBL_OWNER.pbl_autorzy a on za.zaam_am_autor_id=a.am_autor_id
+join IBL_OWNER.pbl_zrodla zr on zr.zr_zrodlo_id=z.za_zr_zrodlo_id
+join IBL_OWNER.pbl_dzialy dz on dz.dz_dzial_id=z.za_dz_dzial1_id
+join IBL_OWNER.pbl_rodzaje_zapisow rz on rz.rz_rodzaj_id=z.za_rz_rodzaj1_id
+where z.za_uzytk_wpis_data between TO_DATE('{DATE_START}','YYYY-MM-DD') and TO_DATE('{date_end}','YYYY-MM-DD')
 and z.za_ro_rok >= 2004
 and z.za_uzytk_wpisal like '{user_pbl}'
 and (z.za_zr_zrodlo_id is not null or z.za_zr_zrodlo_id not like 'x')
@@ -78,7 +161,7 @@ df_ora = df_ora.drop_duplicates()
 
 df_ora
 
-with pd.ExcelWriter("C:\\Users\\PBL_Basia\\Desktop\\Kadry, sprawka, KUP\KUP\\2022\\2022_WRZESIEŃ_WACHEK_BARBARA.xlsx", engine='xlsxwriter') as writer:    
-    df_ora.to_excel(writer, index=False, encoding='utf-8')   
-    writer.save()  
+# with pd.ExcelWriter("C:\\Users\\PBL_Basia\\Desktop\\Kadry, sprawka, KUP\KUP\\2022\\2022_WRZESIEŃ_WACHEK_BARBARA.xlsx", engine='xlsxwriter') as writer:    
+#     df_ora.to_excel(writer, index=False, encoding='utf-8')   
+#     writer.save()  
 
