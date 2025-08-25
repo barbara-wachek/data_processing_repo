6b424b264fcce6d6ff161e8d814f449f55cccf27
Barbara Wachek <barbara.wachek@gmail.com>
2025-08-25 16:07:40 +0200

    KUP

--- KUP.py
+++ KUP.py
@@ -7,35 +7,62 @@ import cx_Oracle
 import pandas as pd
 import os
 import datetime
-import argparse
+import random
+# import argparse
+
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from gspread_dataframe import set_with_dataframe, get_as_dataframe
+import gspread as gs
+
+
 
 
 #%% System variables
 
-# db_password = os.environ.get('DB_PASSWORD')
-# db_user = os.environ.get('DB_USER')
-# db_host = os.environ.get('DB_HOST')
-# db_port = os.environ.get('DB_PORT')
-# db_sid = os.environ.get('DB_SID')
+db_password = os.environ.get('DB_PASSWORD')
+db_user = os.environ.get('DB_USER')
+db_host = os.environ.get('DB_HOST')
+db_port = os.environ.get('DB_PORT')
+db_sid = os.environ.get('DB_SID')
 
 #%% VARIABLES
 
-# user_pbl = input('Wybierz nazwę użytkownika: ')
 date_start = '2020/01/01'
 date_end = datetime.date.today().strftime('%Y/%m/%d')
+
+#%% connect google drive
+
+#autoryzacja do tworzenia i edycji plików
+gc = gs.oauth()
+#autoryzacja do penetrowania dysku
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()
+drive = GoogleDrive(gauth)
 
 
 #%% Functions
 
-def get_user_pbl():
-    parser = argparse.ArgumentParser(description='Test')
-    # Zdefiniuj listę dozwolonych nazw użytkowników
-    dozwolone_nazwy = ['KAROLINA', 'IZA', 'OLA', 'GOSIA', 'BEATAD', 'BEATAS', 'TOMASZ', 'EWA', 'BARBARAW', 'BEATAK', 'NIKODEM', 'CEZARY', 'TOMASZU']
-    parser.add_argument('--user', '-u', type=str, required=True, choices=dozwolone_nazwy,
-                        help='Nazwa użytkownika (dozwolone: {})'.format(', '.join(dozwolone_nazwy)))
-    args = parser.parse_args()
-    return args.user
+def gsheet_to_df(gsheetId, worksheet):
+    gc = gs.oauth()
+    sheet = gc.open_by_key(gsheetId)
+    df = get_as_dataframe(sheet.worksheet(worksheet), evaluate_formulas=True, dtype=str).dropna(how='all').dropna(how='all', axis=1)
+    return df
 
+#Wybór nazwy użytkownika z predefiniowanej listy
+def get_user_pbl():
+    proper_users = ['KAROLINA', 'IZA', 'OLA', 'GOSIA', 'BEATAD', 'BEATAS', 'TOMASZ', 'EWA', 'BARBARAW', 'BEATAK', 'NIKODEM', 'CEZARY', 'TOMASZU']
+
+    while True:
+        answer = input("Wpisz nazwę użytkownika: ")
+        if answer in proper_users:
+            user_pbl = answer
+            print(f"Wybrałeś: {answer}")
+            return user_pbl  # wychodzimy z pętli, bo wpisano poprawną wartość
+        else:
+            print("Błąd: wpisano niedozwoloną wartość. Spróbuj ponownie.")
+        
+        
 
 def connect_to_database():
     """Connects to the Oracle database and returns a connection object."""
@@ -50,56 +77,33 @@ def connect_to_database():
         return None
 
 
-def execute_query(connection, query, user_pbl):
-    """Executes a given SQL query and returns the result as a pandas DataFrame."""
-    df_user = pd.read_sql(query, con=connection)
-    return df_user
+def execute_query(conn, query, param):
+    cursor = conn.cursor()
+    cursor.execute(query, user_pbl)
+    columns = [col[0] for col in cursor.description]
+    df = pd.DataFrame(cursor.fetchall(), columns=columns)
+    cursor.close()
+    return df
 
+def split_dataframe(df, min_rows=180, max_rows=220):
+    """Dzieli DataFrame na mniejsze DataFrame'y o liczbie wierszy z przedziału [min_rows, max_rows]."""
 
-def check_file_exists(file_path):
-    """Sprawdza, czy plik istnieje.
-
-    Args:
-        file_path (str): Ścieżka do pliku.
-
-    Returns:
-        bool: True, jeśli plik istnieje, False w przeciwnym wypadku.
-    """
-
-    if os.path.isfile(file_path):
-        return True
-    else:
-        return False
-
-
-
-# def split_dataframe(df, min_rows=180, max_rows=220):
-#     """Dzieli DataFrame na mniejsze DataFrame'y o określonej liczbie wierszy.
+    result = []
+    start_index = 0
+    n = len(df)
     
-#     Args:
-#       df: DataFrame do podziału.
-#       min_rows: Minimalna liczba wierszy w każdym podziale (domyślnie 180).
-#       max_rows: Maksymalna liczba wierszy w każdym podziale (domyślnie 220).
+    if n <= min_rows:
+        print("Ostrzeżenie: DataFrame jest za mały, aby go podzielić na kilka plików.")
+        return [df]
     
-#     Returns:
-#       Lista podzielonych DataFrame'ów.
-#     """
+    while start_index < n:
+        # losowa liczba wierszy w przedziale min_rows–max_rows, ale nie więcej niż pozostało
+        chunk_size = min(random.randint(min_rows, max_rows), n - start_index)
+        chunk = df.iloc[start_index:start_index + chunk_size]
+        result.append(chunk)
+        start_index += chunk_size
     
-#     result = []
-#     start_index = 0
-#     end_index = min_rows
-    
-#     if len(df) < min_rows:
-#         print("Ostrzeżenie: DataFrame jest za mały, aby go podzielić.")
-#         return [df]
-    
-#     while end_index <= len(df):
-#         chunk = df[start_index:end_index]
-#         result.append(chunk)
-#         start_index = end_index
-#         end_index = min(end_index + min_rows, len(df))
-    
-#     return result
+    return result
 
 
 # def save_dataframes_to_excel(dataframes, user_pbl, output_dir="data", file_format=".xlsx"):
@@ -127,40 +131,45 @@ def check_file_exists(file_path):
 
 
 
-#%% QUERY Zla kolejnosc, gdzie to dac?
-
-query = f'''
-select z.za_zapis_id id, z.za_type typ, rz.rz_nazwa rodzaj, dz.dz_nazwa dzial, a.am_nazwisko autor_nazwisko, a.am_imie autor_imie, z.za_tytul tytul, z.za_opis_wspoltworcow wspoltworcy, z.za_adnotacje adnotacja, zr.zr_tytul czasopismo, z.za_zrodlo_rok rok, z.za_zrodlo_nr numer, z.za_zrodlo_str strony, z.za_uzytk_wpisal, zr.zr_zrodlo_id, z.za_uzytk_wpis_data
-from pbl_zapisy z
-join IBL_OWNER.pbl_zapisy_autorzy za on za.zaam_za_zapis_id=z.za_zapis_id
-join IBL_OWNER.pbl_autorzy a on za.zaam_am_autor_id=a.am_autor_id
-join IBL_OWNER.pbl_zrodla zr on zr.zr_zrodlo_id=z.za_zr_zrodlo_id
-join IBL_OWNER.pbl_dzialy dz on dz.dz_dzial_id=z.za_dz_dzial1_id
-join IBL_OWNER.pbl_rodzaje_zapisow rz on rz.rz_rodzaj_id=z.za_rz_rodzaj1_id
-where z.za_uzytk_wpis_data between TO_DATE('{date_start}','YYYY-MM-DD') and TO_DATE('{date_end}','YYYY-MM-DD')
-and z.za_ro_rok >= 2004
-and z.za_uzytk_wpisal like '{{}}'
-and (z.za_zr_zrodlo_id is not null or z.za_zr_zrodlo_id not like 'x')
-order by z.za_zapis_id
-'''
-
-
 
 #%% Main
-user_pbl = get_user_pbl()
-file_with_user_records = f"data\\ID_records_{user_pbl}.xlsx"
-
 
 if __name__ == "__main__":
+    
+    user_pbl = get_user_pbl()
+    file_with_user_records = '1DpHG81z-HCu1JT4mG2whMxWZ83Zas9j6CerZ36HsGFY'
+
+    query = '''
+    select distinct z.za_zapis_id as ID, 
+           z.za_type as TYP,
+           r.rz_nazwa as RODZAJ, 
+           d.dz_nazwa as DZIAŁ, 
+           t.am_nazwisko as AUTOR_NAZWISKO, 
+           t.am_imie as AUTOR_IMIE, 
+           z.za_tytul as TYTUL, 
+           z.za_adnotacje AS ADNOTACJA, 
+           zr.zr_tytul as CZASOPISMO, 
+           z.za_zrodlo_rok AS ROK, 
+           z.za_zrodlo_nr AS NUMER, 
+           z.za_zrodlo_str AS STRONY, 
+           z.za_uzytk_wpisal AS UZYTKOWNIK   
+    from IBL_OWNER.pbl_zapisy z
+    left join IBL_OWNER.pbl_zrodla zr on z.za_zr_zrodlo_id = zr.zr_zrodlo_id
+    left join IBL_OWNER.pbl_zapisy_autorzy a on z.za_zapis_id = a.zaam_za_zapis_id
+    left join IBL_OWNER.pbl_dzialy d on z.za_dz_dzial1_id = d.dz_dzial_id
+    left join IBL_OWNER.pbl_autorzy t on a.zaam_am_autor_id = t.am_autor_id
+    left join IBL_OWNER.pbl_rodzaje_zapisow r on z.za_rz_rodzaj1_id = r.rz_rodzaj_id
+    where upper(z.za_uzytk_wpisal) like upper(:user_pbl)
+    order by z.za_zapis_id
+    '''
+    
+    
+    
     with connect_to_database() as conn:
         if conn:
-            df = execute_query(conn, query.format(user_pbl), user_pbl)
-            
-            if not check_file_exists(file_with_user_records):
-                print(f"Plik {file_with_user_records} nie istnieje. Program zakończy zadanie.")
-                exit()
+            df = execute_query(conn, query, user_pbl)
                       
-            existing_df = pd.read_excel(file_with_user_records)
+            existing_df = gsheet_to_df(file_with_user_records, 'Arkusz1')
             used_id_list = existing_df['ID'].tolist()
             final_df = df[~df['ID'].isin(used_id_list)]
             
@@ -168,40 +177,31 @@ if __name__ == "__main__":
             # save_dataframes_to_excel(result)
             
             # Zapisz do pliku dataframe ze wszystkimi niewykorzystanymi dotychczas rekordami (zamień potem na format ods)
-            with pd.ExcelWriter(f"data\\{user_pbl}_{datetime.date.today().strftime('%Y-%m-%d')}.xlsx", engine='xlsxwriter') as writer:
-                final_df.to_excel(writer, index=False)
-                writer.close()
             
             
-            # Utwórz nową ramkę danych z nowymi ID
-            final_df_only_ID = pd.DataFrame({'ID': final_df['ID'].tolist()})
-            
-            # Połącz obie ramki danych
-            combined_df = pd.concat([existing_df, final_df_only_ID], ignore_index=True)
-            
-            # Zapisz do pliku, nadpisując istniejący
-            combined_df.to_excel(file_with_user_records, index=False)
-                        
-            
+            if not final_df.empty:
+                # Podział final_df na mniejsze pliki
+                chunks = split_dataframe(final_df, min_rows=180, max_rows=220)
+                
+                for i, chunk in enumerate(chunks, start=1):
+                    filename = f"data\\{user_pbl}_{datetime.date.today().strftime('%Y-%m-%d')}_part{i}.xlsx"
+                    chunk.to_excel(filename, index=False)
+                    print(f"Zapisano plik: {filename}")
+                
+                # Aktualizacja pliku ze wszystkimi ID
+                final_df_only_ID = pd.DataFrame({'ID': final_df['ID'].tolist()})
+                combined_df = pd.concat([existing_df, final_df_only_ID], ignore_index=True)
+                combined_df.to_excel(file_with_user_records, index=False)
+                print(f"Zaktualizowano plik z wszystkimi rekordami: {file_with_user_records}")
+            else:
+                print("Brak nowych rekordów do zapisania.")
         else:
             print("Nie można połączyć się z bazą.")
 
 
 
 
-
-
-
-
-
-
-
-
-
-
 # #%% Query - moj kod
-
-
 
 # #Ponizsze zapytanie generuje wszystkie zapisy danej osoby w okreslonym czasie.
 
@@ -221,27 +221,11 @@ if __name__ == "__main__":
 # '''
 
 
-# df_ora = pd.read_sql(query, con=connection)
-
-
-# test_list_of_real_id = df_ora['ID'][:20].tolist()
-
-# def generate_random_int(length):
-#     """Generuje losową liczbę całkowitą o podanej długości."""
-#     min_val = 10**(length-1)
-#     max_val = 10**length - 1
-#     return random.randint(min_val, max_val)
-
-# test_list_of_random_id = [generate_random_int(7) for x in range(20)]
-# test_list_of_random_id.append(1415923)
-
-
-# df_filtered = df_ora[~df_ora['ID'].isin(test_list_of_random_id)]
 
 
 # #DALEJ: 
 # # filtrownie
-# # identyfikatory not in wcześniej wykorzystane; pliki z już wykorzystanymi identyfikatorami dla każdej osoby oddzielnie 
+# # identyfikatory not in wcześniej wykorzystane; 
 # # plik xlsx → python set()
 # # df_filtered = df[~df['id'].isin(identyfikatory)]
 # # sort → rok, pismo
@@ -250,16 +234,5 @@ if __name__ == "__main__":
 # # zapisać pliki jako .ods z odpowiednimi kolumnami (mniej kolumn niż w zwrotce z sql → z.za_zapis_id id, z.za_type typ, rz.rz_nazwa rodzaj, dz.dz_nazwa dzial, a.am_nazwisko autor_nazwisko, a.am_imie autor_imie, z.za_tytul tytul, z.za_opis_wspoltworcow wspoltworcy, z.za_adnotacje adnotacja, zr.zr_tytul czasopismo, z.za_zrodlo_rok rok, z.za_zrodlo_nr numer, z.za_zrodlo_str strony)
 # # dodać identyfikatory do listy już wykorzystanych
 
-# #Wykorzystać funkcję input? 
 
-# #%% Przetwarzanie danych
-
-# df_ora = pd.read_sql(query, con=connection)
-# df_ora = df_ora.drop_duplicates() 
-
-# df_ora
-
-# # with pd.ExcelWriter("C:\\Users\\PBL_Basia\\Desktop\\Kadry, sprawka, KUP\KUP\\2022\\2022_WRZESIEŃ_WACHEK_BARBARA.xlsx", engine='xlsxwriter') as writer:    
-# #     df_ora.to_excel(writer, index=False, encoding='utf-8')   
-# #     writer.save()  
 
